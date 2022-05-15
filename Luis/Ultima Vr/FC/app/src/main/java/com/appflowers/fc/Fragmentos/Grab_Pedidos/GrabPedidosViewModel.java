04d74e7373c9ec66c72fbfaad76c9e00383ab6ec
luishdez97 <59517366+luishdez97@users.noreply.github.com>
2022-05-15 11:01:40 -0600

    Delete Luis directory

--- Luis/Ultima Vr/FC/app/src/main/java/com/appflowers/fc/Fragmentos/Grab_Pedidos/GrabPedidosViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.appflowers.fc.Fragmentos.Grab_Pedidos;
-
-import androidx.lifecycle.ViewModel;
-
-public class GrabPedidosViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- Luis/Ultima Vr/FC/app/src/main/java/com/appflowers/fc/Fragmentos/Home/HomeViewModel_Fragment.java
+++
@@ -1,7 +0,0 @@
-package com.appflowers.fc.Fragmentos.Home;
-
-import androidx.lifecycle.ViewModel;
-
-public class HomeViewModel_Fragment extends ViewModel {
-    // TODO: Implement the ViewModel
-}
